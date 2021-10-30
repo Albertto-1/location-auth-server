1cf2a95d53e472c650c10edc30f30dbc05132386
Alberto <alberttomarvel@gmail.com>
2021-10-30 01:44:17 -0500

    test

--- utils/auth.py
+++ utils/auth.py
@@ -53,6 +53,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 
 
 async def get_current_user(token: Optional[str] = Depends(oauth2_scheme)):
+    print(token)
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
